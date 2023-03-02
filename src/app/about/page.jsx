0105941ce0232279ab58f559d148d06149602ec2
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-02 15:17:08 -0800

    add navbar component with genre

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function about() {
   return (
-    <div className='max-w-6xl mx-auto space-y-4'>
+    <div className='max-w-6xl mx-auto space-y-4 p-4'>
       <h1 className='text-2xl font-medium text-amber-600'>About</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, sed quasi repellat nam corrupti eaque eius, voluptate quaerat magni laudantium nisi nesciunt nihil at voluptatum ipsa nemo beatae cupiditate omnis?</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, facere consequatur ut temporibus eligendi laudantium animi excepturi nesciunt praesentium minima sed provident repellendus ullam veniam dicta maiores unde voluptatum, sequi, natus accusantium harum fugiat. Rerum?</p>
