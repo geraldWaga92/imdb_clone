9bd3533ad48c77cb558ee50017411699be6c1fba
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-02 14:38:16 -0800

    adding about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, sed quasi repellat nam corrupti eaque eius, voluptate quaerat magni laudantium nisi nesciunt nihil at voluptatum ipsa nemo beatae cupiditate omnis?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, facere consequatur ut temporibus eligendi laudantium animi excepturi nesciunt praesentium minima sed provident repellendus ullam veniam dicta maiores unde voluptatum, sequi, natus accusantium harum fugiat. Rerum?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, at aspernatur quidem atque eveniet, voluptatem dicta velit tenetur unde aliquid officiis esse incidunt laborum modi nihil, voluptatibus ipsa nesciunt sit dignissimos deleniti dolorem ratione accusantium. Et nemo tenetur magnam temporibus reprehenderit consequatur atque veniam eveniet. Unde ratione blanditiis sint mollitia id ex similique neque nobis minus enim. Perferendis, architecto atque.</p>
+    </div>
+  )
+}
