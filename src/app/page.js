aee43370722ebd929c751b808b22e34dda2ca13a
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-02 16:14:32 -0800

    fetch data from TMDB

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Results from "@/components/Results";
+import Data from "@/components/Results";
+import React from "react";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  //if there is no genre use fetchTrending genre
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1,`, { next: { revalidate: 10000 } });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
 
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
   return (
     <div>
-      <h1 className='text-red-400'>Home</h1>
+      <Results results={results} />
     </div>
-  )
+  );
 }
