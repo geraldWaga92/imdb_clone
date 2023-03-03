6cdecd06a4856ec613414c1e3ff72c7e4977d968
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-03 14:23:25 -0800

    add loading spinner effects

---
+++ src/app/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center h-96'>
+      <img src="spinner.svg" alt="loading"  />
+    </div>
+  )
+}
