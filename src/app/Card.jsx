d27f1ae0e46ad23aae746c44f42b9ad40f9e2150
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-06 13:22:34 -0800

    update Card.jsx

--- src/app/Card.jsx
+++ src/app/Card.jsx
@@ -5,9 +5,8 @@ import { FiThumbsUp } from "react-icons/fi"
 
 export default function Card({ result }) {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 m-3 transition-shadow duration-200 group">
       <Link href={`/movie/${result.id}`}>
-        {result.id}
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
@@ -15,19 +14,23 @@ export default function Card({ result }) {
           width={500}
           height={300}
           className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
-          style={{ maxWidth: "100%", height: "auto" }}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
           placeholder="blur"
           blurDataURL="/spinner.svg"
           alt="image is not available"
         ></Image>
         <div className="p-2">
           <p className="line-clamp-2 text-md">{result.overview}</p>
-          <h2 className="truncate text-lg font-bold">{result.title || result.name}</h2>
-          <p className="flex items-center"> {result.release_date || result.first_air_date} 
-            <FiThumbsUp className="h-5 mr-1 ml-3"/> 
-            {result.vote_count}
+          <h2 className="truncate text-lg font-bold">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
           </p>
-          
         </div>
       </Link>
     </div>
