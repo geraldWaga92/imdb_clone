fffe6605f80c9665593bb09a55a14fb19ab98b2a
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-03-06 14:06:09 -0800

    adding search ability

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,29 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+export default function SearchBox() {
+    const [search, setSearch] = useState("");
+    const router = useRouter();
+
+    function handleSubmit(e){
+        e.preventDefault();
+        if(!search) return;
+        router.push(`/search/${search}`);
+    }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex max-w-6xl mx-auto justify-between items-center px-5">
+      <input
+        onChange={e => setSearch(e.target.value)}
+        value={search}
+        type="text"
+        placeholder="Search keywords..."
+        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
+      />
+      <button type="submit" disabled={!search} className="text-amber-600 disabled:text-gray-400">
+        Search
+      </button>
+    </form>
+  );
+}
